c784421dc73cac2e3c595bb8ca1b7146b8d5d6ab
Guerin, Justin <justin.j.guerin@nasa.gov>
2022-08-11 16:01:47 -0400

    first commit

---
+++ PlayGround/PlayGround/PlayGround.cpp
@@ -0,0 +1,69 @@
+// PlayGround.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	double num1, num2;
+	char operatorChoice;
+	double result = 0;
+	bool keepGoing = true;
+	char Exit;
+
+
+	while (keepGoing) {
+		cout << "Enter the first number: ";
+		cin >> num1;
+		cout << "Enter the operator: ";
+		cin >> operatorChoice;
+		cout << "Enter the second number: ";
+		cin >> num2;
+
+		if (operatorChoice == '+')
+		{
+			result = num1 + num2;
+		}
+
+		else if (operatorChoice == '-')
+		{
+			result = num1 - num2;
+		}
+		else if (operatorChoice == '/')
+		{
+			result = num1 / num2;
+		}
+		else if (operatorChoice == '*')
+		{
+			result = num1 * num2;
+		}
+		else {
+			cout << "You screwed up somewhere!";
+		}
+
+		cout << "The result is " << result << endl;
+		cout << "Do you want to keep going? \nEnter Y to continue or anything else to quit: ";
+		cin >> Exit;
+
+		if ((Exit != 'y') && (Exit != 'Y'))
+		{
+			keepGoing = false;
+		}
+		else {
+			system("cls");
+		}
+	}
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
